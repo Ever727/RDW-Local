43e9c02960fe580abc5460debfb108b13b9bd2a7
Ever <cy-huang22@mails.tsinghua.edu.cn>
2024-11-29 15:25:15 +0800

    feat(misc): support angel diff & rotation direction calculations

--- utils/misc.py
+++ utils/misc.py
@@ -1,6 +1,7 @@
 from utils.space import *
 import math
 
+
 def calc_move_with_gain(user, trans_gain, rot_gain, cur_gain_r, cur_direction, delta):
     x = user.x
     y = user.y
@@ -18,5 +19,16 @@ def calc_move_with_gain(user, trans_gain, rot_gain, cur_gain_r, cur_direction, d
 
     x += d_s * math.cos(dir)
     y += d_s * math.sin(dir)
-    
+
     return UserInfo(x, y, dir, user.v, user.w)
+
+
+def calc_min_angel_diff(angle1, angle2):
+    return min(abs(angle1 - angle2), 2 * math.pi - abs(angle1 - angle2))
+
+
+def calc_rotation_direction(target_angle, user_angle):
+    target_angle_diff = target_angle - user_angle
+    a = 1 if target_angle_diff > 0 else -1
+    b = 1 if abs(target_angle_diff) < math.pi else -1
+    return a * b
